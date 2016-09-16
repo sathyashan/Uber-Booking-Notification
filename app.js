a41944fcea8af69e0a920c9363d92e95382497e4
Sathya Moorthy <Sathyamoorthy_s@live.com>
2016-09-16 22:34:37 +0530

    created job on time to be notified

--- app.js
+++ app.js
@@ -10,17 +10,19 @@ app.use(express.static("static"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 //Database
+/*
 postgresHandle.defaults.ssl = true;
 var config = {
-  user: 'jsvjxaxivutiid', 
-  database: 'd3mltsg5qh11fe', 
-  password: 'Iu3pdH2BccKgOWDsmPT_QqPhoD', 
-  host: 'ec2-174-129-209-53.compute-1.amazonaws.com', 
-  port: 5432, 
-  max: 12, 
-  idleTimeoutMillis: 30000
+    user: 'jsvjxaxivutiid',
+    database: 'd3mltsg5qh11fe',
+    password: 'Iu3pdH2BccKgOWDsmPT_QqPhoD',
+    host: 'ec2-174-129-209-53.compute-1.amazonaws.com',
+    port: 5432,
+    max: 12,
+    idleTimeoutMillis: 30000
 };
 var postgresPool = new postgresHandle.Pool(config);
+*/
 //nodemailer config
 var mailer = nodemailer.createTransport({
     service: 'Gmail',
@@ -34,6 +36,29 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+app.post('/api/notify', function (req, res) {
+    console.log(new Date(req.body.notifyTime));
+    scheduler.scheduleJob(new Date(req.body.notifyTime), function () {
+        var mailOptions = {
+            from: 'sathyashan91@gmail.com', // sender address
+            to: req.body.email, // list of receivers
+            subject: 'Reminder - Book a Uber ride', // Subject line
+            html: "<div>Hi " + req.body.name + ",</div><br/><div>Hurry! Its time to book a uber ride from " + req.body.origin + " to " + req.body.destination + "</div><br/>" + "<div>Please do not reply to this mail</div>"
+        };
+        mailer.sendMail(mailOptions, function (mailererr, mailerres) {
+            if (!mailererr) {
+                console.log("email sent");
+            }
+            else {
+                endConnection();
+                console.log("not sent " + mailererr);
+            }
+        });
+    });
+    res.send("success");
+});
+
+/*
 app.post('/api/notify', function (req, res) {
     postgresPool.connect(function (err, client, done) {
         if (!err) {
@@ -59,11 +84,12 @@ app.post('/api/notify', function (req, res) {
         }
 
     });
-});
+});*/
 
 //Scheduler that runs every 1 minute
 //checks for notification in db, if any it sends mail to stored email
-var j = scheduler.scheduleJob('*/1 * * * *', function () {
+/*** 
+var j = scheduler.scheduleJob('* 1 * * * *', function () {
     var d = new Date();
     d.setSeconds(0);
     d.setMilliseconds(0);
@@ -74,7 +100,7 @@ var j = scheduler.scheduleJob('*/1 * * * *', function () {
             client.query("SELECT * FROM UberRemainder WHERE notification_time ='" + d.toUTCString() + "'", function (error, response) {
                 if (!error) {
                     var queryResponse = response.rows, idsString = "";
-                    
+                    console.log(queryResponse);
                     if (queryResponse.length >= 1) {
                         for (var i = 0; i < queryResponse.length; i++) {
                             idsString += (i + 1 === queryResponse.length) ? queryResponse[i].id : (queryResponse[i].id + ",");
@@ -100,7 +126,7 @@ var j = scheduler.scheduleJob('*/1 * * * *', function () {
                                 }
                             });
                         }
-                    }else{
+                    } else {
                         endConnection();
                     }
                 }
@@ -112,7 +138,7 @@ var j = scheduler.scheduleJob('*/1 * * * *', function () {
     });
 
 });
-
+*/
 var port = Number(process.env.PORT || 8092);
 app.listen(port, function () {
     console.log("server running");
